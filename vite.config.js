fc513bfa0e80fbe4972ee96d7c8ecdc06cbfd47d
kxrn0 <kxrp@protonmail.com>
2023-03-10 12:36:29 -0600

    working on implementing PWA

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Pomodoro-App/",
+  // base: "/Pomodoro-App/",
+  plugins: [VitePWA({ registerType: "autoUpdate" })],
 });
