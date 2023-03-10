929d9e7c1b4c648928289ff0f4a1bef3fc82aa1b
kxrn0 <kxrp@protonmail.com>
2023-03-10 17:10:01 -0600

    Solved the issue with the root

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 const options = {
-  base: "/",
+  base: "/Pomodoro-App/",
   includedAssets: ["favicon.ico"],
   manifest: {
     name: "Pomodoro",
@@ -11,7 +11,7 @@ const options = {
     theme_color: "#ffffff",
     background_color: "#ffffff",
     display: "fullscreen",
-    start_url: "/",
+    start_url: "/Pomodoro-App/",
     icons: [
       {
         src: "icon-192x192.png",
